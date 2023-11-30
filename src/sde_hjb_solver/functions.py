bug fixed in gradient of triple well 2d potential + Mueller-Brown 2d + triple well 1d

src/sde_hjb_solver/functions.py
@@ -159,6 +159,37 @@ def skew_double_well_gradient_1d(x):
 
     return 4 * x * (x**2 - 1) - 0.2
 
+def triple_well_1d(x):
+    ''' Asymmetric 1-dimensional triple well
+    '''
+    d = 1
+    x = np.asarray(x)
+
+    # array input
+    if x.ndim == 1:
+        assert x.shape[0] == d, ''
+
+    # batch input
+    elif x.ndim == 2:
+        assert x.shape[1] == d, ''
+
+    return (0.5 * x**6 - 15 * x**4 + 119 * x**2 + 28*x + 50) / 200
+
+def triple_well_gradient_1d(x):
+    ''' Gradient of the asymmetric 1-dimensional triple well.
+    '''
+    x = np.asarray(x)
+
+    # array input
+    if x.ndim == 1:
+        assert x.shape[0] == 1, ''
+
+    # batch input
+    elif x.ndim == 2:
+        assert x.shape[1] == 1, ''
+
+    return + (3 * x**5 - 60 * x**3 + 238 * x + 28) / 200 \
+
 def five_well_1d(x):
     ''' 1-dimensional five well potential.
     '''
@@ -174,8 +205,8 @@ def five_well_1d(x):
         assert x.shape[1] == d, ''
 
     return + (0.5 * x**6 - 15 * x**4 + 119 * x**2 + 28*x + 50) / 200 \
-           - 0.6 * np.exp(-0.5 *(x + 2)**2 / (0.2)**2) \
-           - 0.7 * np.exp(-0.5*(x-1.8)**2/(0.2)**2)
+           - 0.6 * np.exp(-12.5 * (x + 2)**2 ) \
+           - 0.7 * np.exp(-12.5 * (x - 1.8)**2)
 
 def five_well_gradient_1d(x):
     ''' Gradient of the 1-dimensional five well potential.
@@ -191,8 +222,8 @@ def five_well_gradient_1d(x):
         assert x.shape[1] == 1, ''
 
     return + (3 * x**5 - 60 * x**3 + 238 * x + 28) / 200 \
-           - 0.6 * (x + 2) * np.exp(-0.5 *(x + 2)**2 / (0.2)**2) / (0.2)**2 \
-           - 0.7 * (x - 1.8) * np.exp(-0.5*(x - 1.8)**2/(0.2)**2) / (0.2)**2
+           - 15 * (x + 2) * np.exp(-12.5 *(x + 2)**2) \
+           - 17.5 * (x - 1.8) * np.exp(-12.5*(x - 1.8)**2)
 
 def double_well_curved_2d(x):
     '''
@@ -272,7 +303,7 @@ def triple_well_2d(x, alpha):
 
     potential = alpha * (
         + 3 * np.exp(- x[:, 0]**2 - (x[:, 1] - (1./ 3))**2)
-        - 3 * np.exp(- x[:, 0]**2 - (x[:, 1] -(5./3))**2)
+        - 3 * np.exp(- x[:, 0]**2 - (x[:, 1] - (5./3))**2)
         - 5 * np.exp(- (x[:, 0] - 1)**2 - x[:, 1]**2)
         - 5 * np.exp(- (x[:, 0] + 1)**2 - x[:, 1]**2)
         + 0.2 * (x[:, 0]**4)
@@ -306,7 +337,7 @@ def triple_well_gradient_2d(x, alpha):
 
     partial_x = alpha * (
         - 6 * x[:, 0] * np.exp(- x[:, 0]**2 - (x[:, 1] - (1./ 3))**2)
-        - 6 * x[:, 0] * np.exp(- x[:, 0]**2 - (x[:, 1] - (5./3))**2)
+        + 6 * x[:, 0] * np.exp(- x[:, 0]**2 - (x[:, 1] - (5./3))**2)
         + 10 * (x[:, 0] - 1) * np.exp(- (x[:, 0] - 1)**2 - x[:, 1]**2)
         + 10 * (x[:, 0] + 1) * np.exp(- (x[:, 0] + 1)**2 - x[:, 1]**2)
         + (4 / 5) * (x[:, 0]**3)
@@ -314,11 +345,97 @@ def triple_well_gradient_2d(x, alpha):
 
     partial_y = alpha * (
         - 6 * (x[:, 1] - (1./3)) * np.exp(- x[:, 0]**2 - (x[:, 1] - (1./ 3))**2)
-        - 6 * (x[:, 1] - (5./3)) * np.exp(- x[:, 0]**2 - (x[:, 1] - (5./3))**2)
+        + 6 * (x[:, 1] - (5./3)) * np.exp(- x[:, 0]**2 - (x[:, 1] - (5./3))**2)
         + 10 * x[:, 1] * np.exp(- (x[:, 0] - 1)**2 - x[:, 1]**2)
         + 10 * x[:, 1] * np.exp(- (x[:, 0] + 1)**2 - x[:, 1]**2)
         + (4 / 5) * (x[:, 1] - (1./3))**3
     )
+
+    gradient = np.hstack((partial_x, partial_y))
+
+    if is_array_input:
+        gradient = gradient.squeeze()
+
+    return gradient
+
+def mueller_brown_2d(x):
+    '''
+    '''
+    d = 2
+
+    # scalar input
+    if x.ndim == 0:
+        raise ValueError('The input needs to be of array type')
+
+    # array input
+    elif x.ndim == 1:
+        assert x.shape[0] == d, ''
+        x = np.expand_dims(x, axis=0)
+        is_array_input = True
+
+    # batch input
+    elif x.ndim == 2:
+        assert x.shape[1] == d, ''
+        is_array_input = False
+
+    A = [-200, -100, -170, 15]
+    a = [-1, -1, -6.5, 0.7]
+    b = [0, 0, 11, 0.6]
+    c = [-10, -10, -6.5, 0.7]
+    x1_hat = [1, 0, -0.5, -1]
+    x2_hat = [0, 0.5, 1.5, 1]
+
+    K = x.shape[0]
+    potential = np.zeros(K)
+    for i in range(0, 4):
+        potential += A[i] * np.exp(
+            a[i] * (x[:, 0] - x1_hat[i])**2
+            + b[i] * (x[:, 0] - x1_hat[i]) * (x[:, 1] - x2_hat[i])
+            + c[i] * (x[:, 1] - x2_hat[i])**2)
+
+    if is_array_input:
+        potential = potential.squeeze()
+
+    return potential
+
+def mueller_brown_gradient_2d(x):
+    '''
+    '''
+    d = 2
+
+    A = [-200, -100, -170, 15]
+    a = [-1, -1, -6.5, 0.7]
+    b = [0, 0, 11, 0.6]
+    c = [-10, -10, -6.5, 0.7]
+    x1_hat = [1, 0, -0.5, -1]
+    x2_hat = [0, 0.5, 1.5, 1]
+
+    # scalar input
+    if x.ndim == 0:
+        raise ValueError('The input needs to be of array type')
+
+    # array input
+    elif x.ndim == 1:
+        assert x.shape[0] == d, ''
+        x = np.expand_dims(x, axis=0)
+        is_array_input = True
+
+    # batch input
+    elif x.ndim == 2:
+        assert x.shape[1] == d, ''
+        is_array_input = False
+
+    K = x.shape[0]
+    partial_x = np.zeros(K)
+    partial_y = np.zeros(K)
+
+    for i in range(0, 4):
+        exp_term = np.exp(
+            a[i] * (x[:, 0] - x1_hat[i])**2
+            + b[i] * (x[:, 0] - x1_hat[i]) * (x[:, 1] - x2_hat[i])
+            + c[i] * (x[:, 1] - x2_hat[i])**2)
+        partial_x += A[i] * (2 * a[i] * (x[:, 0] - x1_hat[i]) + b[i] * (x[:, 1] - x2_hat[i])) * exp_term
+        partial_y += A[i] * (b[i] * (x[:, 0] - x1_hat[i]) + 2 * c[i] * (x[:, 1] - x2_hat[i])) * exp_term
 
     gradient = np.hstack((partial_x, partial_y))
 